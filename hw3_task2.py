a8b642cb052341beb69ec467731f7219b709e10e
paha85 <padisb@gmail.com>
2022-03-31 08:05:26 +0300

    Задание №1 и 2

--- hw3_task2.py
+++ hw3_task2.py
@@ -12,10 +12,15 @@ words = {
     'thirteen': 'чертова дюжина'
 }
 
+words_cap = {k.capitalize(): v.capitalize() for k, v in words.items()}
 
-def num_translate(i):
-    """Func returns value from dict {words}"""
-    return words.get(i)
+
+def num_translate_adv(i):
+    """Func returns value from dict {words} or {words_cap} for capitalized words"""
+    if i == i.capitalize():
+        return words_cap.get(i)
+    elif i != i.capitalize():
+        return words.get(i)
 
 
 word = (input('Hello, welcome to the translator from English to Russian!\n'
@@ -24,9 +29,9 @@ while True:
     if word == 'quite':
         print('Thanks and goodbye!')
         break
-    elif word not in words.keys():
-        print(f'I\'m sry, I don\'t know word: {word}, so the answer is: {num_translate(word)}')
+    elif word not in words.keys() and word not in words_cap.keys():
+        print(f'I\'m sry, I don\'t know word: {word}, so the answer is: {num_translate_adv(word)}')
         word = (input('\nLets try one more time! \nEnter number from one to ten or quite to exit: '))
     else:
-        print(f'Russian word for: {word} is: {num_translate(word)}')
+        print(f'Russian word for: {word} is: {num_translate_adv(word)}')
         word = (input('\nNice, lets try one more time! \nEnter number from one to ten or quite to exit: '))
