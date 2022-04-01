400d59b414201a9c0e28e37c1bfe7b82d8e1c56c
paha85 <padisb@gmail.com>
2022-04-01 22:35:59 +0300

    Задание 5

--- hw3_task5.py
+++ hw3_task5.py
@@ -8,11 +8,26 @@ adjectives = ["веселый", "яркий", "зеленый", "утопичн�
 def get_jokes(n, flag=False):
     """Function chose 3 random words from lists [nouns], [adverbs], [adjectives]"""
     for i in range(n):
-        noun = choice(nouns)
-        adverb = choice(adverbs)
-        adjective = choice(adjectives)
-        print(noun, adverb, adjective)
+        word_1 = choice(nouns)
+        word_2 = choice(adverbs)
+        word_3 = choice(adjectives)
+        joke = f'{word_1} {word_2} {word_3}'
+        print(joke)
+        duplex = []
+        if flag:
+            duplex = joke.split()
+            for noun in nouns:
+                for punch in duplex:
+                    if noun == punch:
+                        nouns.remove(noun)
+            for adverb in adverbs:
+                for punch in duplex:
+                    if adverb == punch:
+                        adverbs.remove(adverb)
+            for adjective in adjectives:
+                for punch in duplex:
+                    if adjective == punch:
+                        adjectives.remove(adjective)
 
 
-get_jokes(3)
-
+get_jokes(n=3, flag=True)
